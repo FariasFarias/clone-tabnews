d883c8e34c0b3a6dff765e494dcaf5696124d62a
Bianca Farias <88623694+FariasFarias@users.noreply.github.com>
2025-03-13 01:13:50 +0000

    adicionar scripts  e

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
-function Home(){
-    return <h1>Te amo, vida. Se você me ama, me da um beijinho</h1>
+function Home() {
+  return <h1>Te amo, vida. Se você me ama, me da um beijinho</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("teste");
 }
 
 export default Home;
